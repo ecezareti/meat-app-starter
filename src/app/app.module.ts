6e6b57875cf19a4be6764ab2c592cc225d605b74
Carlos Cezareti <edu.cezareti@gmail.com>
2019-03-04 18:22:03 -0300

    internacionalização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { ShoppingCartService } from './restaurant-details/shopping-cart/shopping-cart.service';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { ROUTES } from './app.routes';
 
@@ -19,6 +20,7 @@ import { MenuComponent } from './restaurant-details/menu/menu.component';
 import { MenuItemComponent } from './restaurant-details/menu-item/menu-item.component';
 import { ShoppingCartComponent } from './restaurant-details/shopping-cart/shopping-cart.component';
 import { ReviewsComponent } from './restaurant-details/reviews/reviews.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
@@ -33,14 +35,16 @@ import { ReviewsComponent } from './restaurant-details/reviews/reviews.component
     MenuComponent,
     MenuItemComponent,
     ShoppingCartComponent,
-    ReviewsComponent
+    ReviewsComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [ RestaurantService, ShoppingCartService ],
+  providers: [ RestaurantService, ShoppingCartService, { provide: LOCALE_ID, useValue: 'pt-br'} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { ReviewsComponent } from './restaurant-details/reviews/reviews.component';
+import { OrderComponent } from './order/order.component';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'restaurants', component: RestaurantsComponent },
+  { path: 'order', component: OrderComponent },
   {
     path: 'restaurants/:id', component: RestaurantDetailsComponent,
     children: [
